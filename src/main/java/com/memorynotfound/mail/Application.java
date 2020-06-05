2f6295ef1f2d8f9e4d6c88ec747cfa585e69cb9b
Maciej Kraśkiewicz <maciej.kraskiewicz@ttpsc.pl>
2020-06-05 15:03:44 +0200

    first commit

---
+++ src/main/java/com/memorynotfound/mail/Application.java
@@ -0,0 +1,38 @@
+package com.memorynotfound.mail;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application implements ApplicationRunner {
+
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private EmailService emailService;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        log.info("Sending Email with Thymeleaf HTML Template Example");
+
+        Mail mail = new Mail();
+        mail.setFrom("no-reply@memorynotfound.com");
+        mail.setTo("test@hotmail.com");
+        mail.setSubject("Sending Email with Thymeleaf HTML Template Example");
+
+
+        emailService.sendSimpleMessage(mail);
+    }
+}
